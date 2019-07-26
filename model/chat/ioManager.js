1bda62d871787d712b907b02409ab9ae3a9352de
lilonglong <lilong7676@outlook.com>
2019-07-26 09:33:56 +0800

    socket with token auth

--- model/chat/ioManager.js
+++ model/chat/ioManager.js
@@ -1,14 +1,35 @@
-const ioManager = {
-    initSockeWithServer: function (server) {
-        var io = require('socket.io')(server);
-        io.on('connection', function (socket) {
+class IoManager {
+    constructor(server) {
+        this.io = require('socket.io')(server);
+        this.initIo();
+    }
+
+    initIo() {
+
+        this.io.use((socket, next) => {
+            let handshake = socket.handshake;
+            console.log('io use  handshake', handshake);
+            next();
+        })
+
+        this.io.on('connection', function (socket) {
+
+            socket.use((packet, next) => {
+                console.log('socket use', packet);
+                next();
+            })
+
             socket.emit('msg', { hello: 'world' });
             socket.on('msg', function (data) {
                 console.log(data);
             });
         })
+    }
 
-    },
-};
+    authToken(token) {
 
-module.exports = ioManager;
+    }
+
+}
+
+module.exports = IoManager;
